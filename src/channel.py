3471d8e57c9098fb65b0f6b8e5806caf66d8cf59
Mike Kushner <mike760807@gmail.com>
2023-08-28 23:39:01 +0300

    Edit channel.py

--- src/channel.py
+++ src/channel.py
@@ -18,9 +18,38 @@ class Channel:
         self.title = item["snippet"]["title"]
         self.description = item["snippet"]["description"]
         self.url = "https://www.youtube.com/channel/" + self.channel_id
-        self.subscriber_count = item["statistics"]["subscriberCount"]
-        self.video_count = item["statistics"]["videoCount"]
-        self.view_count = item["statistics"]["viewCount"]
+        self.subscriber_count = int(item["statistics"]["subscriberCount"])
+        self.video_count = int(item["statistics"]["videoCount"])
+        self.view_count = int(item["statistics"]["viewCount"])
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """ * """
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """ - """
+        val1 = self.subscriber_count
+        val2 = other.subscriber_count
+        return val1 - val2
+
+    def __lt__(self, other):
+        """ < """
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """ <= """
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        """ > """
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """ > """
+        return self.subscriber_count >= other.subscriber_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
